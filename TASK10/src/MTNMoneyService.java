2df5f518f441551825c13e43aa8b89b9f5cd24cf
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-04-06 20:25:52 +0300

    commit#1
    In the NV on line 9 i've override the method from the Interface class and ive make it to ensure displaying the attribute declared inside that class

--- TASK10/src/MTNMoneyService.java
+++ TASK10/src/MTNMoneyService.java
@@ -4,4 +4,10 @@ public class MTNMoneyService implements IPaymentService{
     public MTNMoneyService(String clientId) {
         this.clientId = clientId;
     }
+
+    @Override
+    public void sendMoney(String phoneNumber, double amount) {
+        System.out.println("MTN Client ID: " + clientId);
+        System.out.println("Sending " + amount + " UGX to " + phoneNumber + " via MTN Mobile Money.");
+    }
 }
