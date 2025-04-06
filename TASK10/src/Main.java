159ae9ae16516edc1cf27f0ca58f37127e507cd3
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-04-06 20:49:38 +0300

    Commit#1
    In the NV from line 7 to 10 i've created an instance of the first class demonstrating the flexibility of system by injecting

--- TASK10/src/Main.java
+++ TASK10/src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         IPaymentService airtelService = new AirtelMoneyService("AIR123456");
         PaymentProcessor airtelProcessor = new PaymentProcessor(airtelService);
         airtelProcessor.processPayment("O7O2773085", 1000000);
+
+        IPaymentService mtnService = new MTNMoneyService("MTN987654");
+        PaymentProcessor mtnProcessor = new PaymentProcessor(mtnService);
+        mtnProcessor.processPayment("0779123456", 750000);
     }
 }
